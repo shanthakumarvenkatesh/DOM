5e73ede7811d407768831cabe19ca3e3a945d6c7
Shanthakumar <93003141+shanthakumarvenkatesh@users.noreply.github.com>
2022-03-31 15:54:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// var a=document.getElementById("text");
+// console.log(a)
+// // var b=document.getElementsByTagName("h1");
+// // console.log(b);
+// var c=document.getElementsByClassName("box");
+// console.log(c);
+// var b=document.getElementsByTagName("h1");
+// // console.log(b)
+// function change(){
+//     document.getElementById("head").innerHTML="Hello World"
+
+// }
+// function direction(){
+//     document.getElementById("container").style.flexDirection="column"
+// }
+// document.getElementById("head").style.color="red"
+// document.getElementById("head").innerHTML="Heading"
+// function change(){
+//          document.getElementById("head").innerHTML="Welcome to Elevation Academy"
+    
+//     }
+
+// function clock(){
+//     var hours=document.getElementById("hour");
+// var minutes=document.getElementById("min")
+// var seconds=document.getElementById("sec")
+// var time= new Date()
+// var hrs=time.getHours();
+// var mins=time.getMinutes();
+// var secs=time.getSeconds();
+// hours.innerText=hrs;
+// minutes.innerText=mins;
+// seconds.innerText=secs;
+// }
+// setInterval(clock,1000)
+// function getOption() {
+//          var result=document.getElementById("text")
+//          var result2=result.value
+//          document.getElementById("output").innerText=result2
+//     }
